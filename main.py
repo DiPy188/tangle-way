6a3351953f635cefc1be9f16d8bbbd2617aac6a0
SHkipperX <122692826+SHkipperX@users.noreply.github.com>
2023-01-14 23:04:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,244 +8,233 @@ import pygame as pg
 
 colors = pg.Color
 
+group_sprite_green_door = pg.sprite.Group() #Группа зелённых дверей
+group_sprite_red_door = pg.sprite.Group() #Группа красных дверей
+group_sprite_wall = pg.sprite.Group() #группа обычных блоков
+group_sprite_surf = pg.sprite.Group() #Группа поверхности
+all_sprites = pg.sprite.Group()
+player = pg.sprite.Group() #сам игрок
 
-def load_image(name: str, colorkey=None):
-    fullname = os.path.join('data', name)
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data/', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pg.image.load(fullname)
-
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-
     return image
 
 
+
+def generate_level(name):
+    level = list()
+    if os.path.isfile(f'data/' + name):
+        with open(file=f'data/' + name) as r:
+            lines = r.readlines()
+
+        for i in lines:
+            if i != 'pos':
+                level.append(('').join(i.split()))
+
+        #Создание игрового поля
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                if level[y][x] == '0': #0 - трава
+                    Surface('grass', x, y)
+                elif level[y][x] == '1': #1 - стена
+                    Wall('wall', x, y)
+                elif level[y][x] == '2': #2 - зелёная дверь
+                    Green('green_door', x, y)
+                elif level[y][x] == '3': #3 - красная дверь
+                    Red('red_door', x, y)
+
+        Player(lines[-1].split()[0], lines[-1].split()[1]) #сам игрок
+
+objects = {
+    'green_door': load_image('green_door.png'),
+    'red_door': load_image('red_door.png'),
+    'wall': load_image('wall.png'),
+    'grass': load_image('grass.png'),
+    'player': load_image('player.png')
+          }
+tile_width = tile_height = 50 #Размер для отступа от спрайта
+
+
+#Ниже заполнение игрового поля спрайтами
+class Green(pg.sprite.Sprite):
+    def __init__(self, sprite_name, pos_x, pos_y):
+        super().__init__(group_sprite_green_door)
+        self.image = objects[sprite_name]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+class Red(pg.sprite.Sprite):
+    def __init__(self, sprite_name, pos_x, pos_y):
+        super().__init__(group_sprite_red_door)
+        self.image = objects[sprite_name]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, sprite_name, pos_x, pos_y):
+        super().__init__(group_sprite_wall)
+        self.image = objects[sprite_name]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+class Surface(pg.sprite.Sprite):
+    def __init__(self, sprite_name, pos_x, pos_y):
+        super().__init__(group_sprite_surf)
+        self.image = objects[sprite_name]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
 class Player(pg.sprite.Sprite):
-    def __init__(self, *group, pos: Tuple[int, int] = (0, 0), size: Tuple[int, int] = (24, 40)):
-        super(Player, self).__init__(*group)
-
-        self.image = pg.transform.scale(load_image('mar.png'), size)
+    def __init__(self, x, y):
+        super().__init__(player)
+        self.x = int(x)
+        self.y = int(y)
+        self.image = load_image('player.png')
         self.rect = self.image.get_rect()
-        self.rect.topleft = pos
+        self.rect.topleft = (self.x, self.y)
 
 
-class SplashScreen(pg.sprite.Sprite):
-    def __init__(self, *groups, pos: Tuple[int, int] = (0, 0), size: Tuple[int, int] = None):
-        super(SplashScreen, self).__init__(*groups)
-
-        self.image = load_image('fon.jpg')
-        self.image = pg.transform.scale(self.image, size)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = pos
-
-    def delete(self):
-        del self.image
-        del self.rect
-        del self
 
 
-class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        # значения по умолчанию
-        self.left = 0
-        self.top = 0
-        self.cell_size = 50
 
-        self.ground = load_image('grass.png')
-        self.gr_rect = self.ground.get_rect()
-        self.box = load_image('box.png')
-        self.box_rect = self.box.get_rect()
-        self.player = Player()
-        self.load_level('map1.txt')
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
 
-    def get_cell_pos(self, cell_coords: Tuple[int, int]) -> Tuple[int, int]:
-        x, y = cell_coords
-        return x * self.cell_size + self.left, y * self.cell_size + self.top
 
-    def render(self, surface: pg.Surface):
-        for y in range(self.height):
-            for x in range(self.width):
 
-                if self.board[x][y] == 0:
-                    self.gr_rect.topleft = (x * self.cell_size + self.left, y * self.cell_size + self.left)
-                    surface.blit(self.ground, self.gr_rect)
-                elif self.board[x][y] == 1:
-                    self.box_rect.topleft = (x * self.cell_size + self.left, y * self.cell_size + self.left)
-                    surface.blit(self.box, self.box_rect)
-                elif self.board[x][y] == 2:
-                    _x, _y = self.get_cell_pos((x, y))
-                    self.player.rect.topleft = _x + self.cell_size // 2 - 14 // 2, _y + self.cell_size // 2 - 40 // 2
-                    self.gr_rect.topleft = (x * self.cell_size + self.left, y * self.cell_size + self.left)
-                    surface.blit(self.ground, self.gr_rect)
-                    surface.blit(self.player.image, self.player.rect)
 
-    def on_click(self, cell_coords):
-        x, y = cell_coords
-        state = self.board[x][y]
 
-    def get_cell(self, mouse_pos):
-        for y in range(self.height):
-            for x in range(self.width):
-                x1, y1 = x * self.cell_size + self.left, y * self.cell_size + self.top
-                x2, y2 = x1 + self.cell_size, y1 + self.cell_size
 
-                if mouse_pos[0] in range(x1, x2) and mouse_pos[1] in range(y1, y2):
-                    return x, y
-        return
-
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-
-    def load_level(self, name: str):
-        with open(file=os.path.abspath(f'data/{name}'), mode='r', encoding='utf-8') as file:
-            lines = file.readlines()[1:]
-            board = list()
-            for line in lines:
-                line = line[:-1] if line.endswith('\n') else line
-                line: list = line.split(' ')
-                line = list(map(int, line))
-                board.append(line)
-            self.board = board
-            self.width, self.height = len(board), len(board[0])
-
-    def step_left(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[x][y] == 2:
-                    state = self.board[x - 1][y]
-
-                    if x > 0 and state == 0:
-                        self.board[x - 1][y] = 2
-                        self.board[x][y] = state
-                    else:
-                        ...
-
-    def step_right(self):
-        is_continue = True
-
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[x][y] == 2:
-                    state = self.board[x + 1][y]
-
-                    if x < self.width - 1 and state == 0:
-                        self.board[x + 1][y] = 2
-                        self.board[x][y] = state
-                        is_continue = False
-                    else:
-                        ...
-                    if not is_continue:
-                        break
-            if not is_continue:
-                break
-
-    def step_up(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[x][y] == 2:
-                    state = self.board[x][y - 1]
-
-                    if y > 0 and state == 0:
-                        self.board[x][y - 1] = 2
-                        self.board[x][y] = state
-                    else:
-                        ...
-
-    def step_down(self):
-        is_continue = True
-
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[x][y] == 2:
-                    state = self.board[x][y + 1]
-
-                    if y < self.height - 1 and state == 0:
-                        self.board[x][y + 1] = 2
-                        self.board[x][y] = state
-                        is_continue = False
-                    else:
-                        ...
-                    if not is_continue:
-                        break
-            if not is_continue:
-                break
-
-    def update(self):
-        keys = pg.key.get_pressed()
-
-        if keys[pg.K_LEFT]:
-            self.step_left()
-        elif keys[pg.K_RIGHT]:
-            self.step_right()
-        elif keys[pg.K_DOWN]:
-            self.step_down()
-        elif keys[pg.K_UP]:
-            self.step_up()
 
 
 class App:
-    def __init__(self, size: Tuple[int, int] = (600, 600)):
+
+    def __init__(self, size: Tuple[int, int] = (1280, 720)):
         self.is_run = False
         self.is_splash = True
-        self.FPS = 10
+        self.FPS = 40
         self.SIZE = self.WIDTH, self.HEIGHT = size
 
         pg.init()
 
         self.screen = pg.display.set_mode(self.SIZE)
-        self.board = Board(self.WIDTH // 50, self.HEIGHT // 50)
-        self.all_sprites = pg.sprite.Group()
-        self.splash_screen = SplashScreen(self.all_sprites, size=self.SIZE)
         self.clock = pg.time.Clock()
 
     def run(self):
         self.is_run = True
 
+        ''' Открытие БД и проверка, на каком уровне игрок с последующей передачей в | generate_level() | имени уровня'''
+        generate_level('map1.txt')
+
+        all_sprites.add(group_sprite_wall, group_sprite_green_door, group_sprite_red_door, group_sprite_surf, player)
+        all_sprites.draw(self.screen)
+
+        self.c = False #для проверки условий столкнулся спрайт или нет
+
+        self.door_r = True #
+        self.door_g = True #
+
         while self.is_run:
+            keys = pg.key.get_pressed()
             for event in pg.event.get():
+
                 if event.type == pg.QUIT:
                     self.is_run = False
 
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    if self.is_splash:
-                        self.splash_screen.delete()
-                        self.is_splash = False
+            if keys[pg.K_UP] or keys[pg.K_w]:
+                self.Move_pl(y=-1)
 
-            # Update
-            self.all_sprites.update()
-            self.board.update()
+            if keys[pg.K_DOWN] or keys[pg.K_s]:
+                self.Move_pl(y=1)
 
-            # Render
-            self.screen.fill(colors('black'))
+            if keys[pg.K_LEFT] or keys[pg.K_a]:
+                self.Move_pl(x=-1)
 
-            if self.is_splash:
-                self.all_sprites.draw(self.screen)
-            else:
-                self.board.render(self.screen)
+            if keys[pg.K_RIGHT] or keys[pg.K_d]:
+                self.Move_pl(x=1)
+
+            if keys[pg.K_r]:
+                self.lvl_restart()
+
 
             self.clock.tick(self.FPS)
+            all_sprites.draw(self.screen)
             pg.display.flip()
+
         pg.quit()
 
+    def Move_pl(self, x=0, y=0):
+        for i in player:
+            # Прибавляем координаты игрока
+            (x1, y1, x2, y2) = i.rect
+            i.rect = (x1 + x, y1 + y, x2, y2)
+            for j in group_sprite_wall:
+                # Если он пересек стену | c = True |
+                if pg.sprite.collide_mask(i, j):
+                    self.c = True
+                    break
+
+            """Столкновение с дверьми"""
+            if self.door_r == True:
+                for j in group_sprite_red_door:
+                    if pg.sprite.collide_mask(i, j) and self.c is False:
+                        i.rect = (x1 + (75 * x), y1 + y, x2, y2)
+                        self.door_r = False
+                        self.door_g = True
+                        break
+
+            elif self.door_r == False:
+                for j in group_sprite_red_door:
+                    if pg.sprite.collide_mask(i, j) and self.c is False:
+                        i.rect = (x1 - x, y1 - y, x2, y2)
+                        break
+
+
+            if self.door_g == True:
+                for j in group_sprite_green_door:
+                    if pg.sprite.collide_mask(i, j) and self.c is False:
+                        i.rect = (x1 + (75 * x), y1 + y, x2, y2)
+                        self.door_g = False
+                        self.door_r = True
+                        break
+
+            elif self.door_g == False:
+                for j in group_sprite_green_door:
+                    if pg.sprite.collide_mask(i, j) and self.c is False:
+                        i.rect = (x1 - x, y1 - y, x2, y2)
+                        break
+
+            # если условие на пересечение стены выполнено, то возращаем игрока на предыдущую позицию
+            if self.c:
+                (x1, y1, x2, y2) = i.rect
+                i.rect = (x1 - x, y1 - y, x2, y2)
+
+        self.c = False
+
+    def lvl_restart(self):
+        for i in all_sprites:
+            i.kill()
+        for i in group_sprite_wall:
+            i.kill()
+        for i in group_sprite_green_door:
+            i.kill()
+        for i in group_sprite_red_door:
+            i.kill()
+        for i in group_sprite_surf:
+            i.kill()
+        generate_level('map1.txt')
+        all_sprites.add(group_sprite_wall, group_sprite_green_door, group_sprite_red_door, group_sprite_surf,
+                        player)
+        all_sprites.draw(self.screen)
+        self.c = False  # для проверки условий столкнулся спрайт c стеной или нет
+
+        self.door_r = True  #
+        self.door_g = True  #
 
 def main():
     app = App()
